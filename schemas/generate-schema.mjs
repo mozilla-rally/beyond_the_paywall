382753d3d538f44793d88cfac378f0570af55e5d
Brian Chivers <bchivers@stanford.edu>
2021-07-14 13:36:39 -0700

    fixed generate-schema filenames

--- schemas/generate-schema.mjs
+++ schemas/generate-schema.mjs
@@ -98,17 +98,17 @@
  console.log(`generated new advertisement.1.schema.json file in ${__dirname}`);
     
  const articleContent_output = JSON.stringify(articleContent, null, 2);
- fs.writeFileSync(path.join(__dirname, "/articleContent.1.schema.json"), articleContent_output);
+ fs.writeFileSync(path.join(__dirname, "/article-content.1.schema.json"), articleContent_output);
  console.log(`generated new article-content.1.schema.json file in ${__dirname}`);
     
  const pageNav_output = JSON.stringify(pageNav, null, 2);
- fs.writeFileSync(path.join(__dirname, "/pageNav.1.schema.json"), pageNav_output);
+ fs.writeFileSync(path.join(__dirname, "/page-nav.1.schema.json"), pageNav_output);
  console.log(`generated new page-nav.1.schema.json file in ${__dirname}`);
 
  const pageNavSensitive_output = JSON.stringify(pageNavSensitive, null, 2);
- fs.writeFileSync(path.join(__dirname, "/pageNavSensitive.1.schema.json"), pageNavSensitive_output);
+ fs.writeFileSync(path.join(__dirname, "/page-nav-sensitive.1.schema.json"), pageNavSensitive_output);
  console.log(`generated new page-nav-sensitive.1.schema.json file in ${__dirname}`);
 
  const totalTiming_output = JSON.stringify(totalTiming, null, 2);
- fs.writeFileSync(path.join(__dirname, "/totalTiming.1.schema.json"), totalTiming_output);
+ fs.writeFileSync(path.join(__dirname, "/total-timing.1.schema.json"), totalTiming_output);
  console.log(`generated new total-timing.1.schema.json file in ${__dirname}`);
