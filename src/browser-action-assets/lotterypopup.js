8e85662b99d0ba81f71fab870f445acb0808a5b1
Mason Jiang <mpjiang@stanford.edu>
2022-04-12 15:19:17 -0700

    Moved content of browserAction to .html file and filled in redeem code with textContent instead of innerHTML

--- src/browser-action-assets/lotterypopup.js
+++ src/browser-action-assets/lotterypopup.js
@@ -4,8 +4,8 @@
     const codeResults = await browser.storage.local.get(codeStorageKey);
     if(codeResults[codeStorageKey]) {
         const redeemCode = codeResults[codeStorageKey]
-        const contentElement = document.getElementById("content");
-        contentElement.innerHTML = `<h3><center>Beyond the Paywall</center></h3><p>Congratulations, you've won this week's lottery for participants in the Beyond the Paywall study!<br><br>To claim your $50 Amazon.com gift card, use the code <b>${redeemCode}</b><br><br>Please redeem your code at <a href="https://www.amazon.com/gc/redeem">amazon.com/redeem</a> and then click the "Redeemed" button below to clear this message.</p>`
+        const contentElement = document.getElementById("redeemCode");
+        contentElement.textContent = redeemCode
     }
     
     // Listen for clicks on the buttons
