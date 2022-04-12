86ccf11de603dbd842cd653ecd126462f907696b
Mason Jiang <mpjiang@stanford.edu>
2022-04-12 15:18:23 -0700

    Changed console logging protocol, consolidated API call functions, checked for right alarm

--- src/Lottery.js
+++ src/Lottery.js
@@ -31,48 +31,35 @@ import * as webScience from "@mozilla/web-science";
 let initialized = false
 
 /**
- * Fetch lottery results from API
+ * Call API
  * @param {string} userID - The ID of the user.
+ * @param {string} action - Which action to execute ("fetch" or "update")
  */
-async function fetchResults( userID ) {
-  const lotteryurl = "https://oz6ke1zh2h.execute-api.us-west-2.amazonaws.com/default/gjmartin-rally"
-  const response = await fetch(lotteryurl, { // fetch results from API
+async function callAPI( userID, action ) {
+  if (action == "fetch") {
+    url = "https://oz6ke1zh2h.execute-api.us-west-2.amazonaws.com/default/gjmartin-rally"
+  } else if (action == "update") {
+    url = "https://oz6ke1zh2h.execute-api.us-west-2.amazonaws.com/default/gjmartin-rally-redeem"
+  } else {
+    console.error("action is invalid")
+  }
+
+  const response = await fetch(url, { // fetch results or update status from API
     method: 'POST', //
     body: JSON.stringify({"userID":userID})
   });
 
   if (!response.ok) { // there was an error
     const message = "An error has occured: " + response.status;
-    console.log(message)
+    console.error(message)
     return message
   }
 
-  const lotteryresults = await response.json(); // fetch JSON
-  console.log("Result fetched")
-  return lotteryresults;
+  results = await response.json(); // fetch JSON
+  console.debug(action + " executed")
+  return results;
 }
 
-/**
- * Edit code redemption status via API
- * @param {string} userID - The ID of the user.
- */
-async function updateStatus( userID ) {
-  const redemptionurl = "https://oz6ke1zh2h.execute-api.us-west-2.amazonaws.com/default/gjmartin-rally-redeem"
-  const response = await fetch(redemptionurl, { // update status via API
-    method: 'POST', //
-    body: JSON.stringify({"userID":userID})
-  });
-
-  if (!response.ok) { // there was an error
-    const message = "An error has occured: " + response.status;
-    console.log(message)
-    return message
-  }
-
-  const redemptionconfirmation = await response.json(); // fetch JSON
-  console.log("Redemption status updated")
-  return redemptionconfirmation;
-}
 
 /**
  * Set a key-value pair in storage.
@@ -105,60 +92,64 @@ export async function checkWinner ({
 }) {
   // If this module has already been initialized, don't do it again
   if (initialized) {
-    console.log('already')
+    console.debug('already')
     return
   }
   initialized = true
 
   // Set Objects and Parameters
   const surveyUserID = await webScience.userSurvey.getSurveyId() // the User ID
-  console.log("The UserID is " + surveyUserID)
+  console.debug("The UserID is " + surveyUserID)
 
   var lottery_interval = 1440; // how often lottery results checked (in minutes)
   var lottery_delay = 0.5; // when the lottery results are first checked (in minutes)
-  console.log("Lottery will be checked every " + lottery_interval + " minutes")
+  console.debug("Lottery will be checked every " + lottery_interval + " minutes")
   var lotteryAlarm = "lottery-alarm"; // name of lottery alarm
   var winnerNotification = "winner-notification"; // name of winner notification
   browser.alarms.create(lotteryAlarm, {periodInMinutes: lottery_interval, delayInMinutes: lottery_delay}); // create alarm for checking lottery results
 
   browser.alarms.onAlarm.addListener(function(alarm) { // check lottery results when alarm goes off
-    // Run Fetch
-    fetchResults(surveyUserID).then(lotteryresults => {
-      if (lotteryresults['statusCode'] !== 200) { // there was an error
-        console.log("There was an error!")
-      } else if (lotteryresults['data']['isWinner']) { // the user is a winner
-        console.log(surveyUserID + " is a winner!")
+    if (alarm.name == lotteryAlarm) {
+      // Run Fetch
+      callAPI( surveyUserID, "fetch" ).then(lotteryresults => {
+        if (lotteryresults['statusCode'] !== 200) { // there was an error
+          console.error("There was an error!")
+        } else if (lotteryresults['data']['isWinner']) { // the user is a winner
+          console.debug(surveyUserID + " is a winner!")
 
-        // Set Redeem Code
-        const redeemCode = lotteryresults['data']['giftCode']
-        set("redeemCode", redeemCode)
-        console.log("The redeem code is " + redeemCode)
-        
-        // Set Redeem Status
-        const redeemed = lotteryresults['data']['redeemed']
+          // Set Redeem Code
+          const redeemCode = lotteryresults['data']['giftCode']
+          set("redeemCode", redeemCode)
+          console.debug("The redeem code is " + redeemCode)
+          
+          // Set Redeem Status
+          const redeemed = lotteryresults['data']['redeemed']
 
-        // Send Alerts
-        if (!redeemed) { // user has not yet redeemed code
-          // Set up Notification alert
-          browser.notifications.create(winnerNotification, {
-            "type": "basic",
-            "title": "Beyond the Paywall Lottery",
-            "message": "Congratulations, you have won! Click the \"Beyond the Paywall\" extension icon for more details.",
-          });
-          console.log("Notification launched");
+          // Send Alerts
+          if (!redeemed) { // user has not yet redeemed code
+            // Set up Notification alert
+            browser.notifications.create(winnerNotification, {
+              "type": "basic",
+              "title": "Beyond the Paywall Lottery",
+              "message": "Congratulations, you have won! Click the \"Beyond the Paywall\" extension icon for more details.",
+            });
+            console.debug("Notification launched");
 
-          // Set up browserAction alert
-          browser.browserAction.setIcon({path: "src/browser-action-assets/dollar.png"}); // change extension logo from Stanford icon to dollar sign
-          browser.browserAction.setPopup({popup: "src/browser-action-assets/lotterypopup.html"}); // change popup to HTML page
-          console.log("browserAction launched")
-        } else { // user already redeemed code
-          console.log("User already redeeemed code")
-        }
-        
-      } else { // the user did not win
-        console.log(surveyUserID + " did not win.")
-      };
-    });
+            // Set up browserAction alert
+            browser.browserAction.setIcon({path: "src/browser-action-assets/dollar.png"}); // change extension logo from Stanford icon to dollar sign
+            browser.browserAction.setPopup({popup: "src/browser-action-assets/lotterypopup.html"}); // change popup to HTML page
+            console.debug("browserAction launched")
+          } else { // user already redeemed code
+            console.debug("User already redeeemed code")
+          }
+          
+        } else { // the user did not win
+          console.debug(surveyUserID + " did not win.")
+        };
+      });
+    } else {
+      console.debug("Different alarm called")
+    }
   });
 };
 
@@ -180,12 +171,12 @@ export async function redeemCode ({
   // Handle redeemed callback
   webScience.messaging.onMessage.addListener( async () => {
     const surveyUserID = await webScience.userSurvey.getSurveyId() // the User ID
-    console.log("User redeemed code");
-    await updateStatus( surveyUserID ); // edit redemption status via API
+    console.debug("User redeemed code");
+    await callAPI( surveyUserID, "update" ); // edit redemption status via API
     setPopupToNoPromptPage(); // restore browserAction to default
-    console.log("browserAction restored to default")
+    console.debug("browserAction restored to default")
     await browser.notifications.clear("winner-notification"); // clear Notification
-    console.log("Notification cleared")
+    console.debug("Notification cleared")
   }, { type: "WebScience.redeemed" })
 
 };
